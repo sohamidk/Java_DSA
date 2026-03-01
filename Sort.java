11e5b074fb59cc22478a68fc266d75081c367c5a
SOham <soham.a.vade@gmail.com>
2026-03-01 17:38:26 +0530

    version 2

--- Sort.java
+++ Sort.java
@@ -19,4 +19,5 @@ public class Sort{
         }
         printArr(arr);
     }
+    //Bubble sort
 }
---
+++ Sort1.java
@@ -0,0 +1,7 @@
+public class Sort1 {
+    public static void printArr(int arr[]){
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
