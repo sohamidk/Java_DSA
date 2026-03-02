764fff69da8112dba7f57a59f62b19e2ba3beeed
SOham <soham.a.vade@gmail.com>
2026-03-02 18:13:47 +0530

    version 3

---
+++ Recursion3.java
@@ -0,0 +1,16 @@
+public class Recursion3 {
+    public static int calcFact(int n){
+        if( n == 1 || n == 0){
+            return 1;
+        }
+        int calc_nm1 = calcFact(n-1);
+        int fact_n = n * calc_nm1;
+        return fact_n;
+    }
+    public static void main(String args[]){
+        int n = 5;
+        int ans = calcFact(n);
+        System.out.println(ans);
+    }
+    
+}
