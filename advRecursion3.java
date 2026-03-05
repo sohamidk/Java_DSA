93597316a40fb9bdefa8bcb81ff9a40a4ef68e8f
SOham <soham.a.vade@gmail.com>
2026-03-05 22:17:47 +0530

    Advance 4

--- advRecursion3.java
+++ advRecursion3.java
@@ -3,22 +3,24 @@ public class advRecursion3 {
         public static int last = -1;
 
         public static void findOcc(String str, int idx, char element){
-            if(idx == str.length()){
+            if(idx == str.length())
+            {
                 System.out.println(first);
-                System.err.println(last);
+                System.out.println(last);
                 return;
             }
             char currChar = str.charAt(idx);
             if(currChar == element){
                 if(first == -1){
-                    first = idx;
-                }
-                else{
-                    last = idx;
+                    first = -1;
+                }else{
+                    last = -1;
                 }
             }
-            findOcc(str, idx +1, element);
+        
+            findOcc(str, idx + 1, element);
         }
+    
         public static void main(String args[]){
                 String str = "abaacdaaefaah";
              findOcc(str, 0, 'a');
