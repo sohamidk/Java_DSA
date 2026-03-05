10859224e1eeb5be0ea50235109c4dbfe665921a
SOham <soham.a.vade@gmail.com>
2026-03-05 19:56:53 +0530

    Advance 3

--- advRecursion2.java
+++ advRecursion2.java
@@ -1,11 +1,12 @@
 public class advRecursion2 {
+    //String reverse using recursion
     public static void printRev(String str, int idx){
-        if(idx == 0){
-            System.out.print(str.charAt(idx) + " ");
-            return;
-        }
+     if(idx == 0){
         System.out.print(str.charAt(idx) + " ");
-        printRev(str, idx - 1);
+        return ;
+     }
+     System.out.println(str.charAt(idx)+ " ");
+     printRev(str, idx - 1);
     }
     public static void main(String args[]){
         String str = "GitHub";
